6fc835337a9d41878f25dd4720afe5e387d12fe6
mayatm13 <60490093+mayatm13@users.noreply.github.com>
2020-03-08 23:56:55 -0700

    Add files via upload
    YAY task 1 works and I have a test bench

--- memory.sv
+++ memory.sv
@@ -1,13 +1,45 @@
 //Know i is 0 to 255
-module memory(output [7:0] s);
+module memory(output [7:0] data_out, output [7:0] address_out, output logic write_enable, output logic done, input clk, input reset, input en);
 
-logic [31:0] i; 
+//Internal counter
+logic [7:0] i = 8'b0; 
 
-initial begin 
-for (i = 8'b0; i < 8'd256; i = i+ 8'd1) 
-	
-	s[i]<= i;
+always @(posedge clk, posedge reset) begin
 
-end 
+	if (reset == 1'b1)begin
+		i = 8'b0; //Assign counter to zero
+		write_enable = 1'b0;
+		done = 1'b1;
+	end
+	else if (en == 1'b1) begin
+		if (i==8'd255) done= 1'b1;
+		else begin 
+		i = i +8'b1;
+		write_enable = 1'b1;
+		done = 1'b0;
+		end
+	end
+	else if (en == 1'b0) begin
+		i = i; 
+		write_enable = 1'b0;
+		done = 1'b0;
+	end
+	else if (done== 1'b0)begin
+		if (i== 8'd255)begin
+		i= 8'b0;
+		write_enable= 1'b0;
+		done = 1'b1;
+		end 
+		
+		else begin
+		i = i+8'b1;
+		write_enable = 1'b1;
+		done = 1'b0;
+		end 
+	end 
+end
+
+assign address_out = i;
+assign data_out = i; 
 
 endmodule 
