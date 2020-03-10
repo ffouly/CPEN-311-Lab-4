9202f46f32f853af765290760c8483f78f116d77
mayatm13 <60490093+mayatm13@users.noreply.github.com>
2020-03-09 20:23:15 -0700

    Add files via upload

--- ksa.sv
+++ ksa.sv
@@ -9,11 +9,9 @@ module ksa(CLOCK_50, KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ssOut, n
 		output logic [6:0]	ssOut;
 		input logic [3:0]		nIn; 
 
-		wire clk, reset_n, wren, start_shuffle, start_comp, shuffle_finish, comp_finish; 
+		wire clk, reset_n, wren, write_enable_k, enable, end_triggor, done; 
 										
-		wire [7:0] s, address_shuffle, address_comp, data; 
-		wire [31:0] encrypted_input, decrypted_output;
-		wire [7:0]	write_val_shuffle, write_val_comp;
+		wire [7:0] s, address_ram , data_f;  
 		
 	initial begin
 
@@ -21,36 +19,41 @@ module ksa(CLOCK_50, KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ssOut, n
     reset_n <= KEY[3];
 	 
 	 end 
+
+	
+	//This ensures is only called once
+	always_ff @(posedge CLOCK_50, posedge reset_n) begin
+		if (reset_n == 1) begin
+			//done = 1'b0;
+			enable = 1'b0;
+		end
+		else if ((end_triggor == 1'b1) && (done == 1'b0)) begin
+			//done = 1'b1;
+			enable = 1'b1;
+		end
+		else begin
+			enable = 1'b1;
+		//	done = done;
+		end
+	end
+	 	 
+	 memory mem(.data_out(data_f), 
+					.address_out(address_ram), 
+					.write_enable(write_enable_k), 
+					.done(end_triggor), 
+					.clk(CLOCK_50), 
+					.reset(reset_n), 
+					.en(enable));
+	 s_memory mem1(.address(address_ram), 
+						.clock(CLOCK_50), 
+						.data(data_f), 
+						.wren(write_enable_k), 
+						.q(s));
 	 
-	 memory mem(s);
-	 s_memory mem1(.address(address_ram), .clock(CLOCK_50), .data(data), .wren(1'b1), .q(s));
-	 
-	shuffle_array	shuffle( .clk(CLOCK_50),
-									.start_shuffle(start_shuffle),
-									.encrypted_input(encrypted_input), 
-									.s(s), 
-									//.SW(SW), 
-									.wren(wren),
-									.write_val(write_val_comp),
-									.address(address_shuffle), 
-									.decrypted_output(decrypted_output),
-									.shuffle_finish(shuffle_finish));
-				
-			 compute compute( .clk(CLOCK_50),
-									.start_comp(start_comp), 
-									.encrypted_input(encrypted_input), 
-									.s(s),
-									.address(address_comp),
-									.wren(wren),
-									.write_val(write_val_shuffle),
-									.decrypted_output(decrypted_output),
-									.comp_finish(comp_finish));
-									
-			Master		 FSM( .clk(CLOCK_50),
-									.shuffle_finish(shuffle_finish),
-									.comp_finish(comp_finish),
-									.start_shuffle(start_shuffle),
-									.start_comp(start_comp));
-									
+	/*shuffle_array	ahuff(.encrypted_input(encrypted_input),
+								.s(s), 
+							   .SW(SW), 
+								.address(address_ram), 
+							   .decrypted_output(decrypted_output));*/
 
 endmodule 
